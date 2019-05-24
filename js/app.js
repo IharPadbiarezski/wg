6706b62c5c9be38c31dfb1744588a61a05b2433e
IharPadbiarezski <yb.igrok.by@gmail.com>
2019-05-24 19:24:28 +0300

    feat: show order date in format DD/MM/YYYY hh:mm:ss

--- js/app.js
+++ js/app.js
@@ -7,11 +7,13 @@ function loadData() {
 		.then((orders) => {
 			let ordersList = '';
 			orders.forEach((order) => {
+				let orderDate = timeConverter(order.created_at);
+
 				ordersList += `
                     <tr id="order_${order.id}">
                         <td>${order.transaction_id}</td>
                         <td class="user_data">${order.user_id}</td>
-                        <td>${order.created_at} 21/12/2017, 1:13:47 AM</td>
+                        <td>${orderDate}</td>
                         <td>$${order.total}</td>
                         <td>${order.card_number}</td>
                         <td>${order.card_type}</td>
@@ -23,4 +25,19 @@ function loadData() {
 			document.getElementById('orders').innerHTML = ordersList;
 		})
 		.catch((error) => console.log(error));
+}
+
+function timeConverter(UNIX_timestamp) {
+	const a = new Date(UNIX_timestamp * 1000);
+	const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+	const year = a.getFullYear();
+	const month = months[a.getMonth()];
+	const date = a.getDate();
+	let hour = a.getHours();
+	const min = a.getMinutes();
+	const sec = a.getSeconds();
+	let hourNew = hour % 12 || 12;
+	let ampm = hour < 12 ? 'AM' : 'PM';
+	let time = `${date}/${month}/${year}, ${hourNew}:${min}:${sec} ${ampm}`;
+	return time;
 }
